22a988517bf0f842dad4e9249cd1042cb95416d9
ArshiaNarang <70150424+ArshiaNarang@users.noreply.github.com>
2021-01-04 20:53:37 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+
+
+function setup() {
+	createCanvas(1000, 1100);
+	form=new Form()
+	form.display()
+
+}
+
+function draw() {
+    background("white");
+  
+ 
+ 
+  drawSprites();
+ 
+}
+
+
+	
+
+
+
+
+
